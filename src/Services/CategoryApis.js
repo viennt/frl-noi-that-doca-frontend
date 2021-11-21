c04d3cb0102601354ef01d60de8ea61d4f29d7e7
Vien Nguyen <thevien.nguyen@nfq.asia>
2021-11-21 23:31:11 +0700

    [Refactor] using json data

--- src/Services/CategoryApis.js
+++ src/Services/CategoryApis.js
@@ -1,34 +1,19 @@
 import categoriesJson from 'Data/categories.json';
-const url = 'https://api.sheety.co/edbfaff861dbc3a6463ee6b4f446a9b6/noiThatDoca/categories';
 
 export default class CategoryApis {
   static getCategoryBySlug(slug) {
-    if (process.env.NODE_ENV === 'development') {
-      return new Promise((resolve, reject) => {
-        if (!slug) reject()
-        const category = categoriesJson.data
-          .find(category => category.slug === slug);
-        resolve({ data: category })
-      })
-
-    } else {
-      return fetch(`${url}?filter[slug]=${slug}`)
-        .then((response) => response.json())
-        .then(json => ({data: json.categories[0]}));
-    }
+    return new Promise((resolve, reject) => {
+      if (!slug) reject()
+      const category = categoriesJson.data
+        .find(category => category.slug === slug);
+      resolve({ data: category })
+    });
   }
 
   static getCategories() {
-    if (process.env.NODE_ENV === 'development') {
-      return new Promise((resolve) => {
-        const categories = categoriesJson.data;
-        resolve({ data: categories })
-      })
-
-    } else {
-      return fetch(url)
-        .then((response) => response.json())
-        .then(json => ({data: json.categories}));
-    }
+    return new Promise((resolve) => {
+      const categories = categoriesJson.data;
+      resolve({ data: categories })
+    });
   }
 }
--- src/Services/ProductApis.js
+++ src/Services/ProductApis.js
@@ -1,37 +1,22 @@
 import productsJson from 'Data/products.json';
-const url = 'https://api.sheety.co/edbfaff861dbc3a6463ee6b4f446a9b6/noiThatDoca/products';
 
 const PAGE_SIZE = 16;
 
 export default class ProductApis {
   static getProducts(category = 'guong-phong-tam', page = 0) {
-    if (process.env.NODE_ENV === 'development') {
-      return new Promise((resolve) => {
-        const categoryProducts = productsJson.filter(product => product.category === category);
-        const paginationProducts = categoryProducts.slice(page * PAGE_SIZE, page * PAGE_SIZE + PAGE_SIZE);
-        resolve({ data: paginationProducts, length: Math.floor(categoryProducts.length / PAGE_SIZE) })
-      })
-
-    } else {
-      return fetch(url)
-        .then((response) => response.json())
-        .then(json => ({data: json.products}));
-    }
+    return new Promise((resolve) => {
+      const categoryProducts = productsJson.filter(product => product.category === category);
+      const paginationProducts = categoryProducts.slice(page * PAGE_SIZE, page * PAGE_SIZE + PAGE_SIZE);
+      resolve({ data: paginationProducts, length: Math.floor(categoryProducts.length / PAGE_SIZE) })
+    });
   }
 
   static getFeatureProducts(limit = 60) {
-    if (process.env.NODE_ENV === 'development') {
-      return new Promise((resolve) => {
-        const featureProducts = productsJson
-          .filter(product => product.feature)
-          .slice(0, limit);
-        resolve({ data: featureProducts })
-      })
-
-    } else {
-      return fetch(`${url}?filter[feature]=true`)
-        .then((response) => response.json())
-        .then(json => ({data: json.products}));
-    }
+    return new Promise((resolve) => {
+      const featureProducts = productsJson
+        .filter(product => product.feature)
+        .slice(0, limit);
+      resolve({ data: featureProducts })
+    });
   }
 }
