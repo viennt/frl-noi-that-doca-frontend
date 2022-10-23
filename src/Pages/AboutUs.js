b93afe1587359c4126cbe02b2a58f9f140ed3be1
Vu Nguyen <nguyenthevu297@gmail.com>
2022-10-23 16:58:44 +0700

    nav-bar

--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react';
 
 class AboutUs extends PureComponent {
-  render () {
+  render() {
     return (
       <section className="py-4 bg-white sm:py-10 md:px-0">
         <div className="container items-center max-w-7xl px-4 sm:px-6 lg:px-8 mx-auto">
@@ -10,13 +10,13 @@ class AboutUs extends PureComponent {
               <div className="w-full h-full">
                 <img
                   src="/images/giay-chung-nhan.jpeg"
-                  className="object-cover w-full" alt="Khách sạn Heaven - Đà Nẵng"/>
+                  className="object-cover w-full" alt="Khách sạn Heaven - Đà Nẵng" />
               </div>
             </div>
             <div className="relative bg-black shadow-xl border-4 border-black overflow-hidden">
               <div className="w-full h-full">
                 <img src="/images/giay-chung-nhan-doca.jpeg"
-                     className="object-cover w-full" alt=""/>
+                  className="object-cover w-full" alt="" />
               </div>
             </div>
           </div>
