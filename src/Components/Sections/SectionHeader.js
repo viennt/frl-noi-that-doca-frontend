560760792c35835681675002385807b77ae5e7c4
Vu Nguyen <nguyenthevu297@gmail.com>
2022-10-24 09:18:08 +0700

    remove-link

--- src/Components/Sections/SectionHeader.js
+++ src/Components/Sections/SectionHeader.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
 import { Transition } from "@headlessui/react";
 import HeaderMenuItem from 'Components/HeaderMenuItem';
